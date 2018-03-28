682ffd20053cdee4b16522e756471a12d546c762
inponomarev <iponomarev@mail.ru>
2018-03-28 14:19:01 +0300

    fix python

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,7 +4,8 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-curl_cmd = 'curl -o -I -L -s -w "%{http_code}\n"'
+curl_cmd = 'curl -o -I -L -s -w "%{http_code}\n" '
+
 
 def test_webapp_copies(host):
     mellophone = host.file('/opt/tomcat/webapps/mellophone.war')
@@ -18,16 +19,16 @@ def test_service(host):
 
 
 def test_mellophone_greeting(host):
-    cmd = "curl -o -I -L -s -w "%{http_code}\n" 'http://localhost:8080/mellophone'"
+    cmd = curl_cmd + 'http://localhost:8080/mellophone'
     assert host.check_output(cmd) == '200'
     cmd = "curl -L 'http://localhost:8080/mellophone'"
     assert host.check_output(cmd).find(u'Mellophone') > -1
 
 
 def test_mellophone_works(host):
-    cmd = 'curl -o -I -L -s -w "%{http_code}\n"' +\
-          "'localhost:8080/mellophone/checkcredentials?pwd=kulkov&login=kulkov'"
+    cmd = curl_cmd + "'localhost:8080/mellophone/checkcredentials" \
+                     "?pwd=kulkov&login=kulkov'"
     assert host.check_output(cmd) == '200'
-    cmd = 'curl -o -I -L -s -w "%{http_code}\n"' +\
-          "'localhost:8080/mellophone/checkcredentials?pwd=kulkov&login=kulkov2'"
+    cmd = curl_cmd + "'localhost:8080/mellophone/checkcredentials" \
+                     "?pwd=kulkov&login=kulkov2'"
     assert host.check_output(cmd) == '403'
