c7a4bd43565b9879eebcf5c46c6c190b8bfbe251
inponomarev <iponomarev@mail.ru>
2018-03-28 14:11:57 +0300

    modified tests for mellophone

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,12 +4,12 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
+curl_cmd = 'curl -o -I -L -s -w "%{http_code}\n"'
 
 def test_webapp_copies(host):
-    showcase = host.file('/opt/tomcat/webapps/showcase.war')
-    assert showcase.exists
-    assert showcase.is_symlink
+    mellophone = host.file('/opt/tomcat/webapps/mellophone.war')
+    assert mellophone.exists
+    assert mellophone.is_symlink
 
 
 def test_service(host):
@@ -17,8 +17,17 @@ def test_service(host):
     assert tomcat.is_running
 
 
-def test_curl_output(host):
-    cmd = 'curl -o -I -L -s -w "%{http_code}\n" http://localhost:8080/showcase'
+def test_mellophone_greeting(host):
+    cmd = "curl -o -I -L -s -w "%{http_code}\n" 'http://localhost:8080/mellophone'"
     assert host.check_output(cmd) == '200'
-    cmd = 'curl -L http://localhost:8080/showcase'
-    assert host.check_output(cmd).find(u'userdata') > -1
+    cmd = "curl -L 'http://localhost:8080/mellophone'"
+    assert host.check_output(cmd).find(u'Mellophone') > -1
+
+
+def test_mellophone_works(host):
+    cmd = 'curl -o -I -L -s -w "%{http_code}\n"' +\
+          "'localhost:8080/mellophone/checkcredentials?pwd=kulkov&login=kulkov'"
+    assert host.check_output(cmd) == '200'
+    cmd = 'curl -o -I -L -s -w "%{http_code}\n"' +\
+          "'localhost:8080/mellophone/checkcredentials?pwd=kulkov&login=kulkov2'"
+    assert host.check_output(cmd) == '403'
